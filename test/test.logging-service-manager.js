b33b0da67941a73325c8b23f55ef0ae8e2b86923
pahans <pahan123@gmail.com>
2013-10-30 15:57:25 +0530

    logger types unit test

--- test/test.logging-service-manager.js
+++ test/test.logging-service-manager.js
@@ -7,12 +7,15 @@ suite('logging service manager', function() {
 
   test('1 service , 1 logger', function(done) {
     var loggingmanager = new LoggingServiceManager();
-    var logtailmock1 = createLogtailMock(loggingmanager);
+    var logType1 = "type" + Math.random();
+    var logtailmock1 = createLogtailMock(loggingmanager, logType1);
     var logcontent = Math.random();
+
 
     var logginservice1 = createLoggingServiceMock(loggingmanager);
     logginservice1.log = function(type, message) {
       assert.equal(logcontent, message);
+      assert.equal(logType1, type);
       done();
     }
     logtailmock1.writeLog(logcontent);
@@ -20,7 +23,8 @@ suite('logging service manager', function() {
 
   test('2 services , 1 logger', function(done) {
     var loggingmanager = new LoggingServiceManager();
-    var logtailmock1 = createLogtailMock(loggingmanager);
+    var logType1 = "type" + Math.random();
+    var logtailmock1 = createLogtailMock(loggingmanager, logType1);
     var logcontent = Math.random();
     var count = 0;
     var logginservice1 = createLoggingServiceMock(loggingmanager);
@@ -34,6 +38,7 @@ suite('logging service manager', function() {
     var logginservice2 = createLoggingServiceMock(loggingmanager);
     logginservice2.log = function(type, message) {
       assert.equal(logcontent, message);
+      assert.equal(logType1, type);
       count++;
       if (count == 2) {
         done();
@@ -44,61 +49,81 @@ suite('logging service manager', function() {
 
   test('1 service , 2 loggers', function(done) {
     var loggingmanager = new LoggingServiceManager();
-    var logcontent = Math.random();
-    var logtailmock1 = createLogtailMock(loggingmanager);
-    var logtailmock2 = createLogtailMock(loggingmanager);
+    var logType1 = "type" + Math.random();
+    var logType2 = "type" + Math.random();
+    var logcontent1 = Math.random();
+    var logcontent2 = Math.random();
+    var logtailmock1 = createLogtailMock(loggingmanager, logType1);
+    var logtailmock2 = createLogtailMock(loggingmanager, logType2);
 
     var count = 0;
 
     var logginservice1 = createLoggingServiceMock(loggingmanager);
     logginservice1.log = function(type, message) {
-      assert.equal(logcontent, message);
+      if (type == logType1) {
+        assert.equal(logcontent1, message);
+      } else {
+        assert.equal(type, logType2);
+        assert.equal(logcontent2, message);
+      }
       count++;
       if (count == 2) {
         done();
       }
     }
-    logtailmock1.writeLog(logcontent);
-    logtailmock2.writeLog(logcontent);
-
+    logtailmock1.writeLog(logcontent1);
+    logtailmock2.writeLog(logcontent2);
   });
 
   test('2 services , 2 loggers', function(done) {
     var loggingmanager = new LoggingServiceManager();
-    var logcontent = Math.random();
-    var logtailmock1 = createLogtailMock(loggingmanager);
-    var logtailmock2 = createLogtailMock(loggingmanager);
+    var logcontent1 = Math.random();
+    var logcontent2 = Math.random();
+    var logType1 = "type" + Math.random();
+    var logType2 = "type" + Math.random();
+    var logtailmock1 = createLogtailMock(loggingmanager, logType1);
+    var logtailmock2 = createLogtailMock(loggingmanager, logType2);
     var count = 0;
 
     var logginservice1 = createLoggingServiceMock(loggingmanager);
 
     logginservice1.log = function(type, message) {
-      assert.equal(logcontent, message);
-
+      if (type == logType1) {
+        assert.equal(logcontent1, message);
+      } else {
+        assert.equal(type, logType2);
+        assert.equal(logcontent2, message);
+      }
       count++;
-      if (count == 4) {
+      if (count == 2) {
         done();
       }
     }
 
     var logginservice2 = createLoggingServiceMock(loggingmanager);
-    logginservice2.log = function(type, message) {
-      assert.equal(logcontent, message);
 
+    logginservice2.log = function(type, message) {
+      if (type == logType1) {
+        assert.equal(logcontent1, message);
+      } else {
+        assert.equal(type, logType2);
+        assert.equal(logcontent2, message);
+      }
       count++;
-      if (count == 4) {
+      if (count == 2) {
         done();
       }
     }
 
-    logtailmock1.writeLog(logcontent);
-    logtailmock2.writeLog(logcontent);
+    logtailmock1.writeLog(logcontent1);
+    logtailmock2.writeLog(logcontent2);
   });
 
 });
 
-var createLogtailMock = function(loggingmanager) {
+var createLogtailMock = function(loggingmanager, type) {
   var logtailmock = new LogTailerMock();
+  logtailmock.type = type;
   loggingmanager.registerlLogTailer(logtailmock);
   return logtailmock;
 }
